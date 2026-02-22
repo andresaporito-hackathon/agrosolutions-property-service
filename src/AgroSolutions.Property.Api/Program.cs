e16428ebfd17926ebd9f629083b02663156f7418
Andre Saporito <andre.saporito@yahoo.com.br>
2026-02-22 15:31:20 -0300

    feat> k8s

--- src/AgroSolutions.Property.Api/Program.cs
+++ src/AgroSolutions.Property.Api/Program.cs
@@ -67,11 +67,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
